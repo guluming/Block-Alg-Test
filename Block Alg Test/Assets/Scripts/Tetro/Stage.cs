bfc988f5888ef90e782593c03eebee6eb0a2b874
guluming <guluming@naver.com>
2023-01-26 10:09:38 +0900

    드로그 이벤트 테스트 완료 / 그리드 생성 이벤트 테스트 완료

--- Block Alg Test/Assets/Scripts/Tetro/Stage.cs
+++ Block Alg Test/Assets/Scripts/Tetro/Stage.cs
@@ -11,11 +11,24 @@ public class Stage : MonoBehaviour
     public Transform tetrominoNode;
 
     [Header("Game Settings")]
-    [Range(4, 40)]
+    [Range(5, 13)]
     public int boardWidth = 10;
-    [Range(5, 20)]
-    public int boardHeight = 20;
+    [Range(5, 13)]
+    public int boardHeight = 10;
     public float fallCycle = 1.0f;
+
+    private float halfWidth;
+    private float halfHeight;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        halfWidth = (boardWidth * 0.5f);
+        halfHeight = (boardHeight * 0.5f);
+
+        CreateBackground();
+        CreateTetromino();
+    }
 
     Tile CreateTile(Transform parent, Vector2 position, Color color, int order = 1)
     {
@@ -35,38 +48,85 @@ public class Stage : MonoBehaviour
         Color color = Color.gray;
 
         color.a = 0.5f;
-        for (int x = -halfWidth; x < halfWidth; ++x) {
-            for (int y = halfHeight; y > -halfHeight; --y) {
+        for (float x = -halfWidth; x < halfWidth; ++x) {
+            for (float y = halfHeight; y > -halfHeight; --y) {
                 CreateTile(backgroundNode, new Vector2(x, y), color, 0);
             }
         }
-
-        color.a = 1.0f;
-        for (int y = halfHeight; y > -halfHeight; --y) {
-            CreateTile(backgroundNode, new Vector2(-halfWidth - 1, y), color, 0);
-            CreateTile(backgroundNode, new Vector2(halfWidth, y), color, 0);
-        }
-
-        for (int x = -halfWidth - 1; x <= halfWidth; ++x) {
-            CreateTile(backgroundNode, new Vector2(x, -halfHeight), color, 0);
-        }
     }
 
-    private int halfWidth;
-    private int halfHeight;
-
-    // Start is called before the first frame update
-    private void Start()
+    void CreateTetromino()
     {
-        halfWidth = Mathf.RoundToInt(boardWidth * 0.5f);
-        halfHeight = Mathf.RoundToInt(boardHeight * 0.5f);
+        int index = Random.Range(0, 7);
+        Color32 color = Color.white;
 
-        CreateBackground();
-    }
+        tetrominoNode.rotation = Quaternion.identity;
+        tetrominoNode.position = new Vector2(0, halfHeight);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        switch (index)
+        {
+            // I : 하늘색
+            case 0:
+                color = new Color32(115, 251, 253, 255);
+                CreateTile(tetrominoNode, new Vector2(-2f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                break;
+
+            // J : 파란색
+            case 1:
+                color = new Color32(0, 33, 245, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(-1f, 1.0f), color);
+                break;
+
+            // L : 귤색
+            case 2:
+                color = new Color32(243, 168, 59, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 1.0f), color);
+                break;
+
+            // O : 노란색
+            case 3:
+                color = new Color32(255, 253, 84, 255);
+                CreateTile(tetrominoNode, new Vector2(0f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 1f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 1f), color);
+                break;
+
+            // S : 녹색
+            case 4:
+                color = new Color32(117, 250, 76, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, -1f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, -1f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0f), color);
+                break;
+
+            // T : 자주색
+            case 5:
+                color = new Color32(155, 47, 246, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 1f), color);
+                break;
+
+            // Z : 빨간색
+            case 6:
+                color = new Color32(235, 51, 35, 255);
+                CreateTile(tetrominoNode, new Vector2(-1f, 1f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 1f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0f), color);
+                break;
+        }
     }
 }
