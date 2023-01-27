a0f3765e88f17f11f28ce995b94d4572abd45f10
guluming <guluming@naver.com>
2023-01-27 10:49:38 +0900

    Revert "Revert "퍼즐판 위에 퍼즐 올리기 구현""
    This reverts commit c1b884faed4c9dc70af7cf8f76230984d8af3994.

--- Block Alg Test/Assets/Scripts/Game/Grid/GridSquare.cs
+++ Block Alg Test/Assets/Scripts/Game/Grid/GridSquare.cs
@@ -5,16 +5,51 @@ using UnityEngine.UI;
 
 public class GridSquare : MonoBehaviour
 {
+    public Image hooverImage;
+    public Image ActiveImage;
     public Image normalImage;
     public List<Sprite> normalImages;
+
+    public bool Selected { get; set; }
+    public int SquardIndex { get; set; }
+    public bool SquareOccupied { get; set; }
     
     void Start()
     {
-        
+        Selected = false;
+        SquareOccupied = false;
+    }
+
+    public bool CanWeUseThisSquare()
+    {
+        return hooverImage.gameObject.activeSelf;
+    }
+
+    public void ActivateSquare()
+    {
+        hooverImage.gameObject.SetActive(false);
+        ActiveImage.gameObject.SetActive(true);
+        Selected = true;
+        SquareOccupied = true;
     }
 
     public void SetImage(bool setFirstImage)
     {
         normalImage.GetComponent<Image>().sprite = setFirstImage ? normalImages[1] : normalImages[0];
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hooverImage.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        hooverImage.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        hooverImage.gameObject.SetActive(false);
     }
 }
